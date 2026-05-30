18692a354ee9f2bf8912334471abc946b3c9f666
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-30 08:46:39 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class configDenderWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = configDenderWorker()
+    print("Worker engine initialized.")
